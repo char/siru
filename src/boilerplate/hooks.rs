de039abb7e261710e23995abb6afb0ce7371612d
videogame hacker <half-kh-hacker@hackery.site>
2021-05-14 11:40:52 +0100

    Add boilerplate hooks

---
+++ src/boilerplate/hooks.rs
@@ -0,0 +1,19 @@
+use crate::prelude::*;
+use std::path::PathBuf;
+
+pub fn mkdirs_hook(path: PathBuf, contents: Vec<u8>) -> std::io::Result<Vec<(PathBuf, Vec<u8>)>> {
+    let _ = path.parent().map(|parent| std::fs::create_dir_all(parent));
+    Ok(vec![(path, contents)])
+}
+
+pub fn logging_hook(path: PathBuf, contents: Vec<u8>) -> std::io::Result<Vec<(PathBuf, Vec<u8>)>> {
+    // Split and re-join by components since Windows will use a weird mix of slashes
+    let components: Vec<_> = path
+        .components()
+        .map(|c| c.as_os_str().to_string_lossy())
+        .collect();
+
+    log_addition(components.join("/"));
+
+    Ok(vec![(path, contents)])
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod fs;
-pub mod resources;
-
 pub mod logging;
 pub mod markdown;
 pub mod minification;
+pub mod resources;
 pub mod yaml;
+
+pub mod boilerplate;
 
 pub mod prelude;
