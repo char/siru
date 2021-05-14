6e47691dd2c60bc6af45dec12424aa4618a11ce6
videogame hacker <half-kh-hacker@hackery.site>
2021-05-14 19:53:05 +0100

    Import * from submodules in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,10 +1,4 @@
-pub use crate::{
-    fs::{SiruFS, WritePipeline},
-    logging::log_addition,
-    markdown::{parse_frontmatter, render_markdown},
-    minification::minify_html,
-    resources::Resources,
-};
+pub use crate::{fs::*, logging::*, markdown::*, minification::*, resources::*};
 
 pub use askama::Template;
 pub use serde::{Deserialize, Serialize};
